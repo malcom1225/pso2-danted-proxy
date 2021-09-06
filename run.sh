709b6c59896a4aee7748293dbe7ca7f7027cb37c
malcom1225 <55377686+malcom1225@users.noreply.github.com>
2021-09-06 12:05:07 +0700

    apply more sudo

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ cd ./dante-1.4.3
 sudo ./configure --prefix=/usr --sysconfdir=/etc --localstatedir=/var --disable-client --without-libwrap --without-bsdauth --without-gssapi --without-krb5 --without-upnp --without-pam
 sudo make && sudo make install
 cd ..
-cp sockdinit.txt /etc/init.d/sockd
+sudo cp sockdinit.txt /etc/init.d/sockd
 sudo chmod +x /etc/init.d/sockd
 sudo update-rc.d sockd defaults
 
-cp sockdconf.txt /etc/sockd.conf
+sudo cp sockdconf.txt /etc/sockd.conf
 sudo /etc/init.d/sockd start
 sudo /etc/init.d/sockd status
 
